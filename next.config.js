927b2f59e6be6460bdd328cc11a30ca131cb6020
praiseisaac <daramolapraise@gmail.com>
2024-03-24 01:00:23 -0500

    init

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  basePath: "/paalette",
-  output: "export",
   reactStrictMode: false,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
